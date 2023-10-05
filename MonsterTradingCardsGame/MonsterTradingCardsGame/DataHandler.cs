3f5a02e8d17f685a8324a7e773bf3e5ed1fd7416
Dominik Neuwirth <domineu04@gmail.com>
2023-10-05 17:09:45 +0200

    DataHandler

---
+++ MonsterTradingCardsGame/MonsterTradingCardsGame/DataHandler.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonsterTradingCardsGame
+{
+    public class DataHandler
+    {
+        public static DataHandler Instance
+        {
+            get
+            {
+                if (Instance == null)
+                {
+                    Instance = new DataHandler();
+                }
+                return Instance;
+            }
+            private set
+            {
+                Instance = value;
+            }
+        }
+
+        private List<User> users;
+        private List<Card> cards;
+
+        private DataHandler()
+        {
+            //Get all data from db
+            users = new List<User>();
+            cards = new List<Card>();
+        }
+
+        public void Request(string method, string name)
+        {
+
+        }
+
+        private void PostNewCard()
+        {
+
+        }
+    }
+}
